90e6e2d7c3cfd6d416aa70d3f663320ab27d722b
Chad Stark <chad.d.stark@hotmail.com>
2021-01-08 20:56:58 -0800

    Finished button

--- src/app/Components/inputFieldAndButton.js
+++ src/app/Components/inputFieldAndButton.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { Button } from "antd";
-// import { toDoList } from "./listOfStuffToDo";
+
 
 const InputFieldAndButton = () => {
-  const [task, setTask] = useState(true);
+  const [task, setTask] = useState("");
+  const [list, setList] =useState([]);
+  const listItems = list.map((d) => <li key={d.task}>{d.task}</li>)
+  
 
   const handleClick = () => {
-    console.log(task);
+    const newToDo = list.concat({task})
+    setList(newToDo)
   };
-  var testArray = [];
+  console.log(list);
+  
   return (
     <div className="App">
       <h1>Stark's To-Do List</h1>
@@ -17,7 +22,6 @@ const InputFieldAndButton = () => {
           size="100"
           type="text"
           className="toDoInput"
-          // placeholder={useState}
           id="textInput"
           onChange={(event) => setTask(event.target.value)}
         ></input>
@@ -27,7 +31,7 @@ const InputFieldAndButton = () => {
           Add to List
         </Button>
       </div>
-      <div>{testArray}</div>
+      <div>{listItems}</div>
     </div>
   );
 };
