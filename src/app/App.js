db4a634c42fcba6a0c7c024fcb52e30c71e7c1e6
Chad Stark <chad.d.stark@hotmail.com>
2021-01-13 18:49:46 -0800

    complete button almost finished

--- src/app/App.js
+++ src/app/App.js
@@ -6,41 +6,84 @@ import "./App.css";
 import { Button } from "antd";
 import { Card } from "antd";
 
-import {
-  CloseCircleOutlined,
-  CheckCircleOutlined,
-  LoadingOutlined,
-} from "@ant-design/icons";
+import { CloseCircleOutlined, CheckCircleOutlined } from "@ant-design/icons";
 
 const App = () => {
-  // sets state for input field, and sets state for
-  // input value storage to be displayed on screen.
+  // sets state for input field
   const [task, setTask] = useState("");
+  //sets state for object array of inputs
   const [list, setList] = useState([]);
-  // adds the new input item to an array via concat, and
-  //updates the state variable with the new item.
+  //set state for the completed tasks list
+  const [completedList, setCompletedList] = useState([]);
+
+  // adds the new state variable (task) item to an array via concat, and
+  //updates the state (list) variable with the new item.
+  // we also set our completed key to false, for filtering later.
   const handleClick = () => {
-    const newToDo = list.concat({ task });
+    var completed = false;
+    const newToDo = list.concat({
+      task,
+      completed,
+      _id: (Math.random() + 1).toFixed(2),
+    });
     setList(newToDo);
   };
-  // maps over state variable list, and grabs new key items, and displays them.
+
+  const filterListUncomplete = () => {
+    let filterIncomplete = list.filter((x) => x.completed === false);
+    setList(filterIncomplete);
+    console.log(filterIncomplete);
+  };
+
+  //filters out key key values of 'completed' that equal true, stores them into the variable "filterComplete" and that variable is
+  //used to update the state of the completed task list.
+  const filterListComplete = () => {
+    let filterComplete = list.filter((x) => x.completed === true);
+    setCompletedList(filterComplete);
+    console.log(filterComplete);
+  };
+
+  const removeItemList = () => {
+    setList();
+  };
+
+  // maps over state variable (list), and grabs new key items, and displays them.
   const listItems = list.map((d) => (
     <div className="row">
       <div className="col-sm-10">
-        <li className="cardInput" key={d.task}>
+        <li className="uniqueItem" key={d._id.toString()}>
           {d.task}
         </li>
       </div>
       <div className="col-sm-2">
         <span>
-          <button className="btnComplete">
+          <button
+            className="btnComplete"
+            onClick={() => {
+              d.completed = true;
+              filterListUncomplete();
+              filterListComplete();
+            }}
+          >
             <CheckCircleOutlined />
           </button>
-          <button className="btnRemove">
+          <button className="btnRemove" onClick={() => {}}>
             <CloseCircleOutlined />
           </button>
         </span>
       </div>
+    </div>
+  ));
+  console.log(completedList);
+  // maps over the completedlist state, and displays the value of the key "task"
+  const completedListItems = completedList.map((d) => (
+    <div className="row">
+      <div className="col-sm-10">
+        <li className="uniqueItem" key={d._id.toString()}>
+          {d.task}
+        </li>
+      </div>
+      <div className="col-sm-2"></div>
     </div>
   ));
 
@@ -83,6 +126,7 @@ const App = () => {
           type="text"
           className="toDoInput"
           id="textInput"
+          //updates state variable (task) to be the value of the input field
           onChange={(event) => setTask(event.target.value)}
         ></input>
       </div>
@@ -114,10 +158,13 @@ const App = () => {
           <div className="col-sm-6 listComplete">
             <div className="site-card-border-less-wrapper">
               <Card
+                id="completedTaskCard"
                 title="Completed Tasks"
                 border="true"
                 style={{ width: 500, height: 300, marginTop: 50 }}
-              ></Card>
+              >
+                <ul>{completedListItems}</ul>
+              </Card>
             </div>
           </div>
         </div>
